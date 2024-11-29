2de04c19acc723bdfdc809fdd985acc9d0a7f219
Daria Tulickaite <jonshilin.gg@gmail.com>
2024-11-29 21:54:29 +0300

    fixed transformation, added readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# Fractal Flame Generator
+
+![fractal_20241129_210543.png](output%2Ffractal_20241129_210543.png)
+![fractal_20241129_211325.png](output%2Ffractal_20241129_211325.png)
+![fractal_20241129_212435.png](output%2Ffractal_20241129_212435.png)
+![fractal_20241129_212653.png](output%2Ffractal_20241129_212653.png)
+![fractal_20241129_213659.png](output%2Ffractal_20241129_213659.png)
+![fractal_20241129_214132.png](output%2Ffractal_20241129_214132.png)
--- src/main/java/org/example/transformations/nonlinear/PolarTransformation.java
+++ src/main/java/org/example/transformations/nonlinear/PolarTransformation.java
@@ -6,9 +6,9 @@ import org.example.transformations.Transformation;
 public class PolarTransformation implements Transformation {
     @Override
     public Point apply(Point point) {
-        double r = Math.pow(point.x(), 2) + Math.pow(point.y(), 2);
-        double arcTan = Math.atan2(point.y(), point.x());
-        double k = Math.sqrt(r);
-        return new Point(arcTan / Math.PI, k - 1);
+        double r = Math.sqrt(Math.pow(point.x(), 2) + Math.pow(point.y(), 2));
+        double theta = Math.atan2(point.y(), point.x());
+        return new Point(theta / Math.PI, r - 1);
     }
 }
+
